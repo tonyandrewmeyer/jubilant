eda69b3c7e46ca73580dd5e6ad27d6cc01026060
Ben Hoyt <ben.hoyt@canonical.com>
2025-01-27 14:47:54 +1300

    remove a couple more TODOs

--- test/integration/test_basic.py
+++ test/integration/test_basic.py
@@ -3,10 +3,12 @@ import pytest
 import jubilant
 
 
+# NOTE: use jubilant.fixtures model/Juju fixture when ready, see:
+# https://github.com/canonical/jubilant/issues/20
 @pytest.fixture
 def juju():
     j = jubilant.Juju()
-    j.add_model('tt')  # TODO: random model name
+    j.add_model('tt')
     yield j
     j.destroy_model('tt')
 
