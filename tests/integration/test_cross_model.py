4332e37cf4724d0c6b0c3d51e6766e4a815e4dbc
Tony Meyer <tony.meyer@canonical.com>
2025-12-10 12:11:11 +1300

    Go back to looking for the message on the app - we'll wait for Juju to fix the issue where it doesn't update.

--- tests/integration/test_cross_model.py
+++ tests/integration/test_cross_model.py
@@ -14,12 +14,9 @@ def test_offer_and_consume(juju: jubilant.Juju, model2: jubilant.Juju):
     model2.integrate('dbalias', 'testapp')
 
     status = juju.wait(jubilant.all_active)
-    assert status.apps['testdb'].units['testdb/0'].workload_status.message == 'relation created'
+    assert status.apps['testdb'].app_status.message == 'relation created'
 
     status2 = model2.wait(jubilant.all_active, timeout=900)
     assert status2.apps['testapp'].relations['db'][0].related_app == 'dbalias'
-    assert (
-        status2.apps['testapp'].units['testapp/0'].workload_status.message
-        == 'relation changed: dbkey=dbvalue'
-    )
+    assert status2.apps['testapp'].app_status.message == 'relation changed: dbkey=dbvalue'
     assert status2.app_endpoints['dbalias'].relations['db'][0] == 'testapp'
